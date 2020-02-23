2cf7dbb0576b52f30cd41cfb096187d5fb3b35c1
andrealunarojas <59586281+andrealunarojas@users.noreply.github.com>
2020-02-23 01:57:23 -0600

    Add files via upload

--- learn/5.rb
+++ learn/5.rb
@@ -1,12 +1,39 @@
-# Run the code in this file by typing:
+# To run this code, be sure your current working directory
+# is the same as where this file is located.
 # ruby 5.rb
-# into your command-line interface.
 
-better_tacos = ["burgers", "cheese", "tomato", "a", "b"]
-#puts "Bacon #{better_tacos[0]}"
-#puts "Bacon #{better_tacos[1]}"
-#puts "Bacon #{better_tacos[2]}"
+# EXERCISE
+# Build a deck of cards. Given the following arrays, use a loop
+# (or two... hint, hint) to write out every combination to the
+# screen.
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
 
-for food in better_tacos
-    puts "Bacon  #{food}"
+# The simple way
+for rank in ranks
+  for suit in suits
+    puts "#{rank} of #{suit}"
+  end
 end
+
+# Not as simple, but creates a data structure we can use for other things
+
+# Build an empty array
+deck = []
+
+for rank in ranks
+  for suit in suits
+    # adds each card to the array
+    deck << "#{rank} of #{suit}"
+  end
+end
+
+# Now that we have a data structure that represents the "deck";
+# We can "shuffle" the deck
+shuffled_deck = deck.shuffle
+
+# Take one off the top
+puts shuffled_deck[0]
+
+# Or pull out a random card
+puts deck.sample
